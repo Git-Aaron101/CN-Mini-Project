542029333b4bb24286ae46cd0a0087b47a24b38a
Git-Aaron101 <sijo.aaron06@gmail.com>
2026-04-08 18:12:36 +0530

    Updated downloader.py

--- downloader.py
+++ downloader.py
@@ -1,65 +1,42 @@
 import socket
+import ssl
 import time
 
 def download_file():
-    HOST = "example.com"
-    PORT = 80
-import socket
-import time
+    HOST = "127.0.0.1"
+    PORT = 5000
 
-def download_file():
-    HOST = "example.com"   # safer than google
-    PORT = 80
+    context = ssl._create_unverified_context()
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(5)   # IMPORTANT: prevents hanging
-    client.connect((HOST, PORT))
+    secure_client = context.wrap_socket(client, server_hostname=HOST)
 
-    request = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n"
-    client.send(request.encode())
+    secure_client.settimeout(5)
+    secure_client.connect((HOST, PORT))
 
     start = time.time()
 
+    packet_count = 0
+    total_bytes = 0
     data = b''
+
     try:
         while True:
-            chunk = client.recv(4096)
+            chunk = secure_client.recv(4096)
             if not chunk:
                 break
             data += chunk
+            packet_count += 1
+            total_bytes += len(chunk)
     except socket.timeout:
-        # stop if server doesn't close connection
         pass
 
     end = time.time()
-    client.close()
+    secure_client.close()
 
     size = len(data)
     time_taken = end - start if (end - start) > 0 else 0.001
 
     speed = (size * 8) / time_taken / 1000000  # Mbps
 
-    return size, time_taken, speed
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-
-    request = "GET / HTTP/1.1\r\nHost: example.com\r\n\r\n"
-    client.send(request.encode())
-
-    start = time.time()
-
-    data = b''
-    while True:
-        chunk = client.recv(4096)
-        if not chunk:
-            break
-        data += chunk
-
-    end = time.time()
-    client.close()
-
-    size = len(data)
-    time_taken = end - start
-    speed = (size * 8) / time_taken / 1000000  # Mbps
-
-    return size, time_taken, speed
+    return size, time_taken, speed, packet_count
